88e6b6b37907f8257ecc1bc05257a4fb61c456ef
vinicornelius <vinicornelius00@gmail.com>
2021-03-24 23:27:13 -0300

    ok

--- transacao.cpp
+++ transacao.cpp
@@ -1,10 +1,23 @@
+// #  UNIVERSIDADE DA FRONTEIRA SUL
+// #  TRABALHO DE BANCO DE DADOR - TP1
+// #  Autores: Vinicius Cornelius -1811100035 e Leonardo Soares 1811100038  
+
+// #  Tempo de execução com rollback com as 10.000 tuplas  0.98s
+// #  Tempo de execução de inserção  1.318483 min 
+
+// #  Conclusão: Não encontramos o auto commit para a biblioteca libpq-fe que foi 
+// #  usada para realizar a conexão com o banco de dados, portanto toda vez que é 
+// #  realizado um insert, logo em seguido o acontece um commit. Scima estão os tempos
+// #  de execução.
+
+
 #include <iostream>
 #include <stdio.h>
 #include <libpq-fe.h>
 #include <cstring>
 #include <stdlib.h>
 #include <string.h>
-// #include <pqxx/pqxx>
+#include <time.h>
 
 /*Objeto de conexão*/
 PGconn *conn = NULL;
@@ -12,36 +25,53 @@ PGconn *conn = NULL;
 PGresult *result;
 
 
-// char insertWithCommit(char sql){
-//    return false;
-// }
+char insertWithErrosRollback(){
+   PQexec(conn, "INSERT INTO product(eid, description) VALUES (1, stethoscope)");
+   return true;
+}
 
-// char insert(char sql){
-//    return true;
-// }
+char insertWithCommit(char sql[]){
+   PQexec(conn, sql);
+   return true;
+ }
+
 
 int mountSQL(){
    FILE *arq; 
    arq = fopen("data.csv", "r"); 
    int buffer_size = 1024;
    char buffer[1024];
-   int cont = 0;
+   int cont = 25;
+
+   //monitorar o tempo de execução
+   clock_t tempo;
+   tempo = clock();
 
    while (fgets(buffer, buffer_size, arq))
    {
       cont++;
-      char sql[buffer_size] = "INSERT INTO product(description) VALUES ('";
-      char str2[buffer_size] = "');";
-      strcat(sql, buffer);
+      char sql[buffer_size] = "INSERT INTO product(eid, description) VALUES (";
+      char str2[buffer_size] = ", '";
+      char str3[buffer_size] = "');";
+      char szBuffer[1024];
+
+      printf("\n%d\n", cont);
+      snprintf(szBuffer, sizeof (szBuffer), "%d",cont);
+      strcat(sql, szBuffer);
       strcat(sql, str2);
+      strcat(sql, buffer);
+      strcat(sql, str3);
 
       printf("\n%s\n", sql);
       
-      // insertWithCommit(*sql);
+      //inserção 
+      insertWithCommit(sql);
       // insert(sql);
 
    }
-   printf("Total de tuplas \n%d\n", cont);
+   printf("Total de Linhas: %d\n", cont);
+   printf("Tempo :%f \n",(clock() - tempo) / (double)CLOCKS_PER_SEC);
+
    fclose(arq);
    return true;
 }
@@ -57,8 +87,10 @@ int main()
    {
       printf("Conexão efetuada com sucesso. \n");
 
+      //lendo arquivo csv
       mountSQL();
-   
+      //inserção provocando um rollback
+      // insertWithErrosRollback();
    }
    else{
       printf("testando falha ");   
